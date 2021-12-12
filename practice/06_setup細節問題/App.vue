b2e03d9f3224fd1af311ec7fbe1f32258674f4a4
tina <pokemon0850110@gmail.com>
2021-12-12 21:22:07 +0800

    computed&watch&watchEffect

---
+++ practice/06_setup細節問題/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>app</h2>
+  <h3>msg:{{msg}}</h3>
+  <button @click="msg +='==='" >update data</button>
+  <hr/>
+  <Child :msg="msg" @toolabClick="toolabFun" msg2='4545641'/>
+</template>
+<script lang="ts">
+import { defineComponent ,ref} from 'vue'
+//引入子級組件Child
+import Child from './components/Child.vue'
+export default defineComponent({
+  name:'APP',
+
+  //註冊組件
+  components:{
+    Child
+  },
+
+  setup(){
+   
+    const msg=ref('what are you')
+    function toolabFun(key:string){
+      console.log('aaa'+key)
+      msg.value+=key
+    }
+    return{
+      msg,
+      toolabFun
+    }
+  }
+})
+</script>
---
+++ practice/06_setup細節問題/components/Child.vue
@@ -0,0 +1,48 @@
+<template>
+<h2>child</h2>
+<h3>msg:{{msg}}</h3>
+<button @click="emitXxx">分發事件</button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name:'Child',
+    props:['msg'],
+    
+
+    //數據初始化的生命週期回調
+    beforeCreate(){
+        console.log('beforecreate')
+    },
+
+    //setup會在beforeCreate前就執行了
+    //setup在執行的時候 當前組件還沒有創建出來 this不能用會undefined
+
+    setup(props,{attrs,slots,emit}){
+    //setup(props,context){
+        
+        /*context參數 是一個對象 裡面有
+         attrs對象(獲取當前組件標籤上的所有屬性的對象 但是該屬性是在props中沒有聲明接收的對象)
+         emit方法 
+         slots對象
+        */
+       // console.log(context.attrs.msg2)
+       // console.log(context)
+        console.log(attrs.msg2)
+
+
+        //按鈕的點擊事件的回調函數
+        function emitXxx(){
+            console.log('aaa')
+          //  context.emit('toolabClick','+=+');  
+          emit('toolabClick','+=+')
+        }
+        
+
+        console.log('setup')
+        return{
+            emitXxx
+        }
+    }
+})
+</script>
